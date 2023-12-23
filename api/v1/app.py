6054284a3e7443a0dfde44b2034e93348e4b249a
CaloMorell <oscarmorell07@yahoo.com>
2023-12-22 22:52:20 -0400

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,43 +1,33 @@
 #!/usr/bin/python3
-""" Flask Application """
-from models import storage
-from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
+"""
+The RESTful api starts here. The api aids data access in the app.
+"""
+from os import getenv
+
+from flask import Flask, jsonify
 from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
+
+from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+CORS(app, origins=["0.0.0.0"])
+host = getenv("HBNB_API_HOST", "0.0.0.0")
+port = getenv("HBNB_API_PORT", "5000")
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
+def teardown(exception):
+    """Cleanup operations"""
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-    404:
-        description: a resource was not found
-    """
-    return make_response(jsonify({'error': "Not found"}), 404)
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host, port, threaded=True, debug=True)
