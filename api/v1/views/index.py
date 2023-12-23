ee231355d6dcb8b7a856d8226a02e8bc09b7b6b7
CaloMorell <oscarmorell07@yahoo.com>
2023-12-22 23:04:01 -0400

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Index of views"""
-
-
+"""
+Views index, contains status and stat endpoints.
+"""
+from api.v1.views import app_views
+from flask import jsonify
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -9,24 +11,30 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from models import storage
-from api.v1.views import app_views
-from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User,
+}
+
+
+@app_views.route("/status")
 def status():
-    """ Status of API """
+    """Status of my API"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def number_objects():
-    """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+@app_views.route("/stats")
+def stats():
+    """Retrieve the number of each objects by type"""
+    statistics = {}
 
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
+    for key, value in classes.items():
+        statistics[key] = storage.count(value)
 
-    return jsonify(num_objs)
+    return jsonify(statistics)
